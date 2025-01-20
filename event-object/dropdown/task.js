35b8d6f83b226b636c7accf7a5ede94ef27d6ce6
elinaulli <162158747+elinaulli@users.noreply.github.com>
2025-01-20 09:11:01 +0500

    Исправлено

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,20 +1,16 @@
 let dropdownItems = Array.from(document.querySelectorAll(".dropdown__item")); //массив из возможных вариантов;
 let dropdownList = document.querySelector(".dropdown__list");
 
-function openMenu() {
-	dropdownList.classList.add("dropdown__list_active");
-}
 
-function closeMenu() {
-	dropdownList.classList.remove("dropdown__list_active");
-}
 
 const value = document.querySelector(".dropdown__value");
-value.addEventListener("click", openMenu);
+value.addEventListener("click", () => {
+	dropdownList.classList.toggle("dropdown__list_active");
+});
 
 dropdownItems.forEach((item, index) => {
 	item.onclick = function() {
-		closeMenu();
+		dropdownList.classList.toggle("dropdown__list_active");
 		value.textContent = dropdownItems[index].textContent;
 		return false; // Возвращение return false из обработчика события предотвращает действие браузера по умолчанию
 
